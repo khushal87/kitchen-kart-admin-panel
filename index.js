e5c5db266361bd850cfb6acac998db626023c34b
khushal87 <hrithik.agarwal87@gmail.com>
2020-08-03 23:28:21 +0530

    initial commit

--- index.js
+++ index.js
@@ -10,17 +10,17 @@ server.set('views', path.join(__dirname, '/views'))
 const apiRoute = require('./routes/API/index.js')
 
 server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({extended:true}));
+server.use(bodyParser.urlencoded({ extended: true }));
 
 server.use('/', express.static(path.join(__dirname, '/public')))
 server.use('/api', apiRoute.route);
-server.get('/download/:id', function(req, res){
+server.get('/download/:id', function (req, res) {
     let id = req.params.id;
     res.download(`./${id}.xlsx`);
 })
 
 const port = process.env.PORT || 3000;
 
-server.listen(port, function(){
+server.listen(port, function () {
     console.log(`Server up and running on port: ${port}`);
 })
